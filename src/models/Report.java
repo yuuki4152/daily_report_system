4b7062dd21255823f3ac6522fe563cd5ced1a563
yuuki4152 <keiko102010131127@gmail.com>
2021-07-24 18:36:19 +0900

    Modify top page

--- src/models/Report.java
+++ src/models/Report.java
@@ -18,13 +18,21 @@ import javax.persistence.Table;
 @Table(name = "reports")
 @NamedQueries({
     @NamedQuery(
-        name = "getAllReports",
-        query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
-    ),
+            name = "getAllReports",
+            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+            ),
     @NamedQuery(
-        name = "getReportsCount",
-        query = "SELECT COUNT(r) FROM Report AS r"
-    ),
+            name = "getReportsCount",
+            query = "SELECT COUNT(r) FROM Report AS r"
+            ),
+    @NamedQuery(
+            name = "getMyAllReports",
+            query = "SELECT r FROM Report AS r WHERE r.employee = :employee ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getMyReportsCount",
+            query = "SELECT COUNT(r) FROM Report AS r WHERE r.employee = :employee"
+            ),
 })
 @Entity
 public class Report {
